53d4abd33af9866cd280a12695c066a191d98d87
Georges Nodari <ngeorges.dev@gmail.com>
2025-08-21 03:16:59 +0200

    feat: update ESLint configuration to disable specific TypeScript rules for better flexibility

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,15 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 })
 
-const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript")]
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+    },
+  },
+]
 
 export default eslintConfig
