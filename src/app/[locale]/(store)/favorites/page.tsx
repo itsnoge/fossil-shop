81870986e13284900c5cce5e7343d59aa7205933
Georges Nodari <ngeorges.dev@gmail.com>
2025-08-26 14:40:54 +0200

    feat: update Favorites page with translations and improved layout; enhance descriptions in English and French

--- src/app/[locale]/(store)/favorites/page.tsx
+++ src/app/[locale]/(store)/favorites/page.tsx
@@ -1,8 +1,23 @@
-export default function Favorites() {
+import { getTranslations } from "next-intl/server"
+
+type Props = {
+  params: Promise<{ locale: string }>
+}
+
+export default async function Favorites({ params }: Props) {
+  const { locale } = await params
+
+  const tSection = await getTranslations("Sections")
+  const tBrand = await getTranslations("Metadata.favorites")
+
   return (
-    <div>
-      <h1>Your Favorites</h1>
-      <p>Here you can find all your favorite items.</p>
+    <div className="font-sans lg:container lg:mx-auto">
+      <div className="mb-20">
+        <h1 className="font-figtree mb-10 text-4xl font-semibold md:text-7xl">
+          {tSection("favorites")}
+        </h1>
+        <p className="max-w-md font-sans font-medium">{tBrand("description")}</p>
+      </div>
     </div>
   )
 }
